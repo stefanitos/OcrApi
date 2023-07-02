b4cd26e5429dafcc49db822012e47e650de4697c
stefanitos <stefanmuscat5@gmail.com>
2023-07-02 16:09:16 +0200

    Smite Ocr api done 1

--- app.py
+++ app.py
@@ -1,18 +1,84 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, status
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, UploadFile, HTTPException, File
+from pydantic import BaseModel
+from typing import List
+from PIL import Image
 import pytesseract
-
+import cv2
+import numpy as np
+import io
+import uvicorn
+import time
 
 app = FastAPI()
 
+class Player(BaseModel):
+    ally: List[str]
+    enemy: List[str]
 
-@app.get("ocr/upload")
-async def upload(request: Request):
-    # Get the image from the request and check if its allowed ['png', 'jpg', 'jpeg']
-    image = request.files["image"]
-    if image.filename.split(".")[-1] not in ["png", "jpg", "jpeg"]:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid file type")
-    # Read the image and convert it to text
-    text = pytesseract.image_to_string(image.file.read())
+def get_player_names(img: Image) -> Player:
+    offset_x = 180
+    offset_y_ally = 75
+    offset_y_enemy = 580
+    width = 150
+    height = 24
+    gap = 200
 
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    img = cv2.bitwise_not(img)
+    img = Image.fromarray(img)
+
+    ally_players_coords = [(offset_x + (width + gap) * i, offset_y_ally, offset_x + (width + gap) * i + width, offset_y_ally + height) for i in range(5)]
+    enemy_players_coords = [(offset_x + (width + gap) * i, offset_y_enemy, offset_x + (width + gap) * i + width, offset_y_enemy + height) for i in range(5)]
+    all_players_coords = ally_players_coords + enemy_players_coords
+
+    players = {
+        "ally": [],
+        "enemy": []
+    }
+
+    for i, box in enumerate(all_players_coords):
+        cropped_img = img.crop(box)
+        text = pytesseract.image_to_string(cropped_img, config="--psm 7").strip()
+
+        if text:
+            if i < 5:
+                players["ally"].append(text)
+            else:
+                players["enemy"].append(text)
+
+    return Player(**players)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/ocr/smite/upload")
+async def smite_upload(file: UploadFile = File(...)):
+    start_time = time.time()
+    try:
+        contents = await file.read()
+        img = Image.open(io.BytesIO(contents))
+        players = get_player_names(img)
+        end_time = time.time()
+        time_taken = end_time - start_time
+        return {"players": players, "time_taken": time_taken}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid image file.")
     
+
+@app.post("/ocr/upload")
+async def ocr_upload(file: UploadFile = File(...)):
+    start_time = time.time()
+    try:
+        contents = await file.read()
+        img = Image.open(io.BytesIO(contents))
+        text = pytesseract.image_to_string(img, config="--psm 7")
+        end_time = time.time()
+        time_taken = end_time - start_time
+        return {"text": text, "time_taken": time_taken}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid image file.")
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- test.py
+++ test.py
@@ -1,40 +1,19 @@
-from pytesseract import image_to_string
-from PIL import Image
-import cv2
-import time
+import requests
 
-start_time = time.time()
-
-offset_x = 180
-offset_y_ally = 75
-offset_y_enemy = 580
-width = 150
-height = 24
-gap = 200
-
-img = cv2.imread("loading_screen.png")
-img = cv2.bitwise_not(img)
-img = Image.fromarray(img)
-
-ally_players_coords = [(offset_x + (width + gap) * i, offset_y_ally, offset_x + (width + gap) * i + width, offset_y_ally + height) for i in range(5)]
-enemy_players_coords = [(offset_x + (width + gap) * i, offset_y_enemy, offset_x + (width + gap) * i + width, offset_y_enemy + height) for i in range(5)]
-all_players_coords = ally_players_coords + enemy_players_coords
-
-players = {
-    "ally": [],
-    "enemy": []
-}
-
-for i, box in enumerate(all_players_coords):
-    cropped_img = img.crop(box)
-    # cropped_img.save(f"players/player{i+1}.png")
-    text = image_to_string(cropped_img, config="--psm 7").strip()
-
-    if text:
-        if i < 5:
-            players["ally"].append(text)
-        else:
-            players["enemy"].append(text)
+# Specify the URL
+url = 'http://127.0.0.1:8000/smite/ocr/upload'
 
 
-print(players)
+bin_img = open('loading_screen.png', 'rb')
+
+# Dict with post data
+data = {"file": bin_img}
+
+# Make the request
+response = requests.post(url, files = data)
+
+# Close the file to free resources
+bin_img.close()
+
+# Print the response
+print(response.json())
